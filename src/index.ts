2aeda98f1eda39304aa2797f4c728adc01622f86
Jongmin Park <m1089724@gmail.com>
2025-06-20 11:11:44 +0900

    integrate Google Calendar client into Hono app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { Hono } from 'hono';
+import {
+	createCalendarClient,
+	type CalendarClient,
+} from '@/common/utils/google-calendar';
+import { getValidatedEnv } from '@/common/utils/env';
 
-const app = new Hono();
+const app = new Hono<{
+	Variables: {
+		calendarClient: CalendarClient;
+	};
+}>();
+
+app.use('*', async (c, next) => {
+	const env = getValidatedEnv(c);
+	const client = createCalendarClient(
+		env.GOOGLE_CLIENT_EMAIL,
+		env.GOOGLE_PRIVATE_KEY
+	);
+
+	c.set('calendarClient', client);
+	await next();
+});
 
 app.get('/', (c) => {
 	return c.text('Hello');
